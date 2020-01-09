403ba51957fc3e14577bef2b9dd6196712f9da89
Michael Lomnitz <mllomnitz@gmail.com>
2020-01-09 15:54:17 -0800

    Adds utils to imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from .models import *
 from .dataset import *
 from .train import *
+from .utils import *
 
-__all__ = [*models.__all__, *dataset.__all__, *train.__all__]
+__all__ = [*models.__all__, *dataset.__all__, *train.__all__, *utils.__all__]
