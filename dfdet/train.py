4032631634bb288da36895d9ef657d9f9ce7962d
Michael Lomnitz <mllomnitz@gmail.com>
2020-01-23 10:43:26 -0800

    Adds additional checkpointing, and new direcvtory per model

--- dfdet/train.py
+++ dfdet/train.py
@@ -1,9 +1,11 @@
 from tqdm import tqdm
+from datetime import datetime
+from pathlib import Path
 
 import torch
 import torch.nn as nn
 
-from .utils import save_checkpoint
+from .utils import save_checkpoint, plot_losses
 
 __all__ = ['train_dfd', 'test_dfd']
 
@@ -120,6 +122,10 @@ def train_dfd(model=None, dataloader=None, testloader=None, optim=None,
     model = model.to(device)
     meter = AverageMeter()
     best_loss = 999
+    now = datetime.now()
+    chpt_dest = './checkpoints_{}'.format(now.strftime("%d-%m-%Y_%H:%M:%S"))
+    Path(chpt_dest).mkdir(parents=True, exist_ok=True)
+    test_losses = []
     if verbose is False:
         pbar = tqdm(total=len(dataloader))
     for epoch in range(n_epochs):
@@ -147,16 +153,27 @@ def train_dfd(model=None, dataloader=None, testloader=None, optim=None,
                 pbar.update(1)
         acc, v_loss = test_dfd(dataloader=testloader, model=model,
                                criterion=criterion, device=device)
+        test_losses.append(v_loss)
+        if (epoch+1) % 5 == 0:
+            save_checkpoint(model, 'Epoch {} Train:{} Test:{} '.format(epoch, meter.avg, v_loss),
+                            '{}/model_epoch_{}.pth.tar'.format(chpt_dest, epoch+1))
         if v_loss < best_loss:
             best_loss = v_loss
-            save_checkpoint(model, 'best model loss: {}'.format(v_loss),
-                            './checkpoints/best_model.pth.tar')
+            save_checkpoint(model, 'best model Train:{} Test: {}'.format(meter.avg, v_loss),
+                            '{}/best_model.pth.tar'.format(chpt_dest))
         print('Epoch {} \n Train loss:{:.4f} \n Test accuracy:{:.4f} loss:{:.4f}'.format(
             epoch, meter.avg, acc, v_loss))
         if verbose is False:
-            pbar.reset(0)
+            pbar.refresh()
+            pbar.reset()
         if scheduler is not None:
             scheduler.step(meter.avg)
             averages.append(meter.avg)
             meter.reset()
     pbar.close()
+    plot_losses(averages, test_losses, chpt_dest)
+    save_checkpoint(
+        model=model,
+        description='Final Train:{} Test:{}'.format(
+            averages[-1], test_losses[-1]),
+        filename='{}/final_model.pth.tar'.format(chpt_dest))
